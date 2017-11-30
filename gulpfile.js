f0b982e37c03bcebca4dde651f02f536c50257ed
cipionhabla <cipionhabla@gmail.com>
2017-11-29 22:21:01 -0500

    Added validate and different plugins
    Check if forms are valid and optimized files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
-// Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
@@ -12,3 +14,11 @@ gulp.task('sass', function(){
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
 })
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
